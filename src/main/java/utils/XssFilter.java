6d1ad36c0b8b75e46b9ece2c361df7ab60ac2208
Kirbinator-rgb <dc84q45@gmail.com>
2026-08-09 05:56:35 -0400

    probe: restore url access, poor validation and nosql; revert the xss filter

--- src/main/java/utils/XssFilter.java
+++ src/main/java/utils/XssFilter.java
@@ -28,107 +28,168 @@ public class XssFilter {
 
   private static final Logger log = LogManager.getLogger(XssFilter.class);
 
-  private static final String FALLBACK_URL =
-      "https://www.google.com/search?q=What+does+a+HTTP+link+look+like";
-
   /**
-   * Validates a URL for use in an href attribute.
+   * A method to badly validate a URL
    *
    * @param input URL to validate
-   * @return An http(s) URL encoded for an HTML attribute, or a fallback URL
+   * @return A poorly validated URL (XSS RISK)
    */
   public static String anotherBadUrlValidate(String input) {
-    return validateHttpUrl(input);
+    String howToMakeAUrlUrl =
+        new String("https://www.google.com/search?q=What+does+a+HTTP+link+look+like");
+    input = input.toLowerCase();
+    if (input.startsWith("http")) {
+      try {
+        URL theUrl =
+            new URL(
+                input
+                    .replaceAll("#", "&#x23;")
+                    .replaceFirst("<", "&#x3c;")
+                    .replaceFirst(">", "&#x3e;")
+                    .replaceFirst("\"", "&quot;"));
+        input = theUrl.toString();
+      } catch (MalformedURLException e) {
+        log.debug("Could not Cast URL from input: " + e.toString());
+        input = howToMakeAUrlUrl;
+      }
+    } else {
+      log.debug("Was not a HTTP URL");
+      input = howToMakeAUrlUrl;
+    }
+    return input;
   }
 
   /**
-   * Validates a URL for use in an href attribute.
+   * White lists for specific URL types but doesn't sanitise it well
    *
-   * @param input URL to validate
-   * @return An http(s) URL encoded for an HTML attribute, or a fallback URL
+   * @param input
+   * @return
    */
   public static String badUrlValidate(String input) {
-    return validateHttpUrl(input);
-  }
-
-  /**
-   * Parses the input as a URL and admits only the http and https schemes, then encodes the result
-   * for the attribute context it is written into. Blacklisting characters could not work here:
-   * anything that survives the filter still lands unencoded inside href="...".
-   *
-   * @param input URL to validate
-   * @return An encoded http(s) URL, or an encoded fallback URL when input is not one
-   */
-  private static String validateHttpUrl(String input) {
-    if (input == null) {
-      return Encode.forHtmlAttribute(FALLBACK_URL);
-    }
-    try {
-      URL theUrl = new URL(input.trim());
-      String protocol = theUrl.getProtocol();
-      if (!"http".equals(protocol) && !"https".equals(protocol)) {
-        log.debug("Rejected URL with non-HTTP(S) scheme: " + protocol);
-        return Encode.forHtmlAttribute(FALLBACK_URL);
+    String howToMakeAUrlUrl =
+        new String("https://www.google.com/search?q=What+does+a+HTTP+link+look+like");
+    input = input.toLowerCase();
+    if (input.startsWith("http")) {
+      try {
+        URL theUrl =
+            new URL(
+                input
+                    .replaceAll("#", "&#x23;")
+                    .replaceAll("<", "&#x3c;")
+                    .replaceAll(">", "&#x3e;")
+                    .replaceFirst("\"", "&quot;"));
+        input = theUrl.toString();
+      } catch (MalformedURLException e) {
+        log.debug("Could not Cast URL from input: " + e.toString());
+        input = howToMakeAUrlUrl;
       }
-      return Encode.forHtmlAttribute(theUrl.toString());
-    } catch (MalformedURLException e) {
-      log.debug("Could not cast URL from input: " + e.toString());
-      return Encode.forHtmlAttribute(FALLBACK_URL);
+    } else {
+      log.debug("Was not a HTTP URL");
+      input = howToMakeAUrlUrl;
     }
+    return input;
   }
 
   /**
-   * Encodes input for the HTML body and attribute contexts it is written into.
+   * Encodes for HTML, but doesn't escape ampersands
    *
-   * @param input String to be encoded
-   * @return HTML-encoded input
+   * @param input
+   * @return
    */
   public static String encodeForHtml(String input) {
-    log.debug("Encoding input for HTML");
-    return Encode.forHtml(input);
+    log.debug("Filtering input at XSS white list");
+
+    input = Encode.forHtml(input);
+    // Decode quotes to open a security hole in Encoder
+    input = input.replaceFirst("&#34;", "\"");
+    // Encode lower-case "on" and upper-case "on" to complicate the required attack vectors to pass
+    return input.replaceAll("on", "&#x6f;&#x6e;").replaceAll("ON", "&#x4f;&#x4e;");
   }
 
   /**
-   * Encodes input for the HTML body context it is written into.
+   * Filters for specific javascript events recursively in a specific order. Can be bypassed by
+   * embedding a trigger late in the list in a trigger early in the list
    *
-   * @param input String to be encoded
-   * @return HTML-encoded input
+   * @param input String to be filtered for XSS attacks
+   * @return XSS Blacklist filtered HTML
    */
   public static String levelFour(String input) {
-    log.debug("Encoding input at XSS levelFour");
-    return Encode.forHtml(input);
+    String[] javascriptTriggers = FindXSS.javascriptTriggers;
+    log.debug("Filtering input at XSS levelFour");
+    input = input.toLowerCase();
+    while (input.contains("script")) {
+      System.out.println("input = " + input);
+      input = input.replaceAll("script", "scr.pt");
+    }
+    for (int i = 0; i < javascriptTriggers.length; i++) {
+      while (input.contains(javascriptTriggers[i])) {
+        int len = javascriptTriggers[i].length();
+        String replacement =
+            javascriptTriggers[i].substring(0, (len / 2) - 1)
+                + "."
+                + javascriptTriggers[i].substring((len / 2) + 1, len);
+        input = input.replaceAll(javascriptTriggers[i], replacement);
+      }
+    }
+    return screwHtmlEncodings(input);
   }
 
   /**
-   * Encodes input for the HTML body context it is written into.
+   * Filters the word "script" specifically
    *
-   * @param input String to be encoded
-   * @return HTML-encoded input
+   * @param input Input to be filtered for XSS
+   * @return XSS Blacklist filtered HTML
    */
   public static String levelOne(String input) {
-    log.debug("Encoding input at XSS levelOne");
-    return Encode.forHtml(input);
+    log.debug("Filtering input at XSS levelOne");
+    return input.toLowerCase().replaceAll("script", "scr.pt").replaceAll("SCRIPT", "SCR.PT");
   }
 
   /**
-   * Encodes input for the HTML body context it is written into.
+   * Filters for javascript triggers twice before stopping and breaks HTML encodings
    *
-   * @param input String to be encoded
-   * @return HTML-encoded input
+   * @param input
+   * @return
    */
   public static String levelThree(String input) {
-    log.debug("Encoding input at XSS levelThree");
-    return Encode.forHtml(input);
+    log.debug("Filtering input at XSS levelThree");
+    input = input.toLowerCase();
+    input = input.replaceAll("script", "scr.pt");
+    for (int h = 0; h < FindXSS.javascriptTriggers.length; h++) {
+      for (int i = 0; i <= 1; i++) {
+        input = input.replaceAll(FindXSS.javascriptTriggers[h], "");
+      }
+    }
+    return screwHtmlEncodings(input);
   }
 
   /**
-   * Encodes input for the HTML body context it is written into.
+   * Filters specific javascript event triggers
    *
-   * @param input String to be encoded
-   * @return HTML-encoded input
+   * @param input String to be filtered for XSS attacks
+   * @return XSS Blacklist filtered HTML
    */
   public static String levelTwo(String input) {
-    log.debug("Encoding input at XSS levelTwo");
-    return Encode.forHtml(input);
+    input = input.toLowerCase();
+    log.debug("Filtering input at XSS levelTwo");
+    input = input.replaceAll("script", "scr.pt");
+    input = input.replaceAll("onclick", "o.ick");
+    input = input.replaceAll("onmouseover", "o.ver");
+    input = input.replaceAll("onload", "o.oad");
+    input = input.replaceAll("onerror", "o.err");
+    input = input.replaceAll("ondblclick", "o.dbl");
+    return screwHtmlEncodings(input);
+  }
+
+  /**
+   * Use this to cripple HTML encoded attacks. This is can be used to limit the vectors of attack
+   * for success
+   *
+   * @param input The string you want to remove HTML encoding from
+   * @return A string without HTML encoding
+   */
+  private static String screwHtmlEncodings(String input) {
+    input = input.replaceAll("&", "!").replaceAll(":", "!");
+    return input;
   }
 }
